9e037c88f835d30e816e1f8ed8795e576d9c38ae
brantmerrell <brant.merrell@gmail.com>
2017-10-24 07:53:00 +0000

    longer webscraping loop

--- scrapeLoop.R
+++ scrapeLoop.R
@@ -9,10 +9,30 @@ arguments <- seq(arguments[1], arguments[length(arguments)])
 
 DF <- scrape.chess.com(arguments[1])
 
+print(DF$ID)
+
 for(n in arguments[-1]){
 	DF <- weave.rbind(DF,scrape.chess.com(n))
+	print(tail(DF$ID,3))
 }
 
-filename <- paste("data/", min(arguments),"-",  max(arguments),".csv", sep="")
+TEST <- grepl("thinkboolean", DF$White)
+
+TEST <- TEST | grepl("thinkboolean", DF$Black)
+
+if(0<sum(TEST)){
+	DF1 <- DF[TEST,]
+	print(tail(DF1))
+	if(file.exists("../thinkboolean.csv")){
+		DF1 <- weave.rbind(DF1, read.csv("../thinkboolean.csv"))
+	}
+	write.csv(DF1, "../thinkboolean.csv", row.names=F)
+}
+
+filename <- paste("data/chess.com IDs ", min(arguments),"-",  max(arguments),".csv", sep="")
 
 write.csv(DF, filename, row.names=F)
+
+print(filename)
+
+
--- scrapeLoop.sh
+++ scrapeLoop.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-while getopts f:l: o
-do	case "$o" in 
-	f)	first="$OPTARG";;
-	l)	last="$OPTARG" 
-	esac
-done
-
-Rscript scrapeLoop.R $((first)),$((last))
-
-#aws s3 sync data s3://jbchess/data
-
-aws s3 mv data s3://jbchess/data --recursive
-
-aws s3 ls s3://jbchess/data --recursive
-#for n in $(seq $((first)) $((last)));
-#	do Rscript test.R $((n));
+#while getopts f:l: o
+#do	case "$o" in 
+#	f)	first="$OPTARG";;
+#	l)	last="$OPTARG" 
+#	esac
 #done
 
+#Rscript scrapeLoop.R $((first)),$((last))
 
+for n in $(seq 0 99)
+do
+	first=$(($n*1000+1))
+	last=$(($n*1000+1000))
+	Rscript scrapeLoop.R $first,$last
+	aws s3 mv data s3://jbchess/data --recursive
+	echo date
+done
+
+#aws s3 ls s3://jbchess/data --recursive
