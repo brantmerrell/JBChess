20cb36020aeb5f805a928c305cf7b00b7df5f0fe
Ubuntu <ubuntu@ip-172-31-42-59.ec2.internal>
2018-01-31 18:07:44 +0000

    update textConnection to regex

--- R/chesscolor.R
+++ R/chesscolor.R
@@ -1,11 +1,11 @@
 chesscolor <- function(pgn){
-	if(ncol(read.table(textConnection(pgn),sep="."))==4){
-		return("black")
-	}
-	if(ncol(read.table(textConnection(pgn),sep="."))==2){
-		return("white")
-	}
-	if(!(ncol(read.table(textConnection(pgn),sep=".")) %in% c(2,4))){
-		stop("invalid number of dots")
-	}
+	if(grepl("(?<!\\.)\\.{3}(?!\\.)",pgn,perl=T)){return("black")}
+	if(grepl("(?<!\\.)\\.{1}(?!\\.)",pgn,perl=T)){return("white")}
+	return(NA)
 }
+
+# testing:
+print(paste("one dot - expect white:", chesscolor("1.e3")))
+print(paste("two dots - expect NA:",chesscolor("1..e6")))
+print(paste("three dots - expect black:", chesscolor("1...e6")))
+print(paste("four dots - expect NA:", chesscolor("1....e3")))
