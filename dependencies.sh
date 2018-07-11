d311bd1138225a47da9e29c2d2c5d86b6a6e254e
Joshua Brant Merrell <brant.merrell@gmail.com>
2018-07-11 00:27:08 -0400

    cleanup

--- dependencies.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# sudo apt-get install build-essential libcurl4-gnutls-dev libxml2-dev libssl-dev
-
-# R -e "install.packages("devtools")"
-
-R -e "install.packages('jsonlite', dependencies=TRUE, repos='http://cran.rstudio.com/')"
