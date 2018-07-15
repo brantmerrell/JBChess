78f1a5adacd43cab471b70a0f66d73de59878fc8
Joshua Brant Merrell <brant.merrell@gmail.com>
2018-07-15 19:51:36 -0400

    update board.R

--- R/board.R
+++ R/board.R
@@ -1,13 +1,8 @@
-board <- function (game_pgn = nrow(position), command = "View,return",wordSep = " ") {
-	DF <- data.frame(matrix(position[game_pgn, ], nrow = 8, ncol = 8), 
-		row.names = 8:1)
-	colnames(DF) <- letters[1:8]
-	DF <- apply(DF, c(1, 2), gsub, pattern = " ", replacement = wordSep)
-	dimnames(DF) <- list(8:1, letters[1:8])
-	if (grepl("View", command)) {
-		View(DF)
-	}
-	if (grepl("return", command)) {
-		return(as.matrix(DF))
-	}
+board <- function (position_vec) {
+  if(class(position_vec)=="data.frame"){
+    position_vec <- unlist(position_vec[nrow(position_vec),])
+  }
+	DF <-matrix(position_vec, nrow = 8, ncol = 8, 
+	                 dimnames = list(8:1, letters[1:8]))
+	DF
 }
