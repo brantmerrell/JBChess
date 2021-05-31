f39279e331cd180d3e73b8c674610d640e122293
brantmerrell <ec2-user@ip-172-31-31-17.us-east-2.compute.internal>
2021-05-31 06:40:49 +0000

    commence major cleanup

--- R/helpers/Notation/pgnPiece.R
+++
@@ -1,8 +0,0 @@
-pgnPiece <- function(pgn){
-  test <- unlist(lapply(chesspatterns, grepl, x = pgn))
-  test <- names(test)[test]
-	test <- test[test %in% c("queen","king", "knight", "rook", "bishop", "pawn")]
-	if(length(test)>1){stop("pgn tests positive for multiple pieces")}
-	if(length(test)<1){stop("pgn test negative for all pieces")}
-	test
-}
--- R/helpers/Position/piecePost..R
+++
@@ -1,9 +0,0 @@
-piecePost. <- function(square,game_pgn){
-	piece <- as.vector(position[game_pgn,square])
-	if(grepl(chesspatterns$pawn,piece)){return(pawnPost.(square,game_pgn))}
-	if(grepl(chesspatterns$knight,piece)){return(paste("",knightPost.(square,game_pgn),sep=""))}
-	if(grepl(chesspatterns$bishop,piece)){return(paste("",bishopPost.(square,game_pgn),sep=""))}
-	if(grepl(chesspatterns$rook,piece)){return(paste("",rookPost.(square,game_pgn),sep=""))}
-	if(grepl(chesspatterns$queen,piece)){return(paste("",queenPost.(square,game_pgn),sep=""))}
-	if(grepl(chesspatterns$king,piece)){return(paste("",kingPost.(square,game_pgn),sep=""))}
-}
